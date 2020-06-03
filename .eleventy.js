423f199ed09f3eb9a8eac60922ac89e5c622d9e9
Jason Pamental <jason@rwt.io>
2020-06-03 09:39:06 -0400

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,69 @@
+
+const Image = require("@11ty/eleventy-img");
+
+module.exports = function(eleventyConfig) {
+
+
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  
+
+  eleventyConfig.addLiquidShortcode("footnote", function(number, chapter) { 
+    return `<a href="#ch${chapter}-fn${number}" id="ch${chapter}-fnref${number}" class="footnote--reference">${number}</a>`;
+  });
+
+  // works also with addLiquidShortcode or addNunjucksAsyncShortcode
+  eleventyConfig.addLiquidShortcode("myResponsiveImage", async function(src, alt, options) {
+      let stats = await Image(src, {
+        // Array of widths
+        // Optional: use falsy value to fall back to native image size
+        widths: [480, 900, 1200, null],
+      
+        // Pass any format supported by sharp
+        formats: ["jpeg"], //"png"
+      
+        // the directory in the image URLs <img src="/img/MY_IMAGE.png">
+        urlPath: "/assets/respimg/",
+      
+        // the path to the directory on the file system to write the image files to disk
+        outputDir: "assets/respimg/",
+      
+        // eleventy-cache-assets
+        // If a remote image URL, this is the amount of time before it downloads a new fresh copy from the remote server
+        cacheDuration: "1d"
+      });
+      let lowestSrc = stats.jpeg[0];
+      let sizes = "(min-width: 550px) 900px, (min-width: 950px) 1200px, (min-width: 1300px) 1600px, 100vw"; // Make sure you customize this!
+      let loadingLazy = options;
+      if(loadingLazy === 'lazy') {
+        loadingLazyString = 'loading="lazy"';
+      } else {
+        loadingLazyString = '';
+      }
+
+      if(alt === undefined) {
+        // You bet we throw an error on missing alt (alt="" works okay)
+        throw new Error(`Missing \`alt\` on myResponsiveImage from: ${src}`);
+      }
+
+      // Iterate over formats and widths
+      return `<img 
+        ${Object.values(stats).map(imageFormat => {
+          return ` srcset="${imageFormat.map(entry => `${entry.url} ${entry.width}w`).join(", ")}" sizes="${sizes}" alt="${alt}"
+          src="${lowestSrc.url}" width="${lowestSrc.width}" height="${lowestSrc.height}"
+          ${loadingLazyString} `;
+        }).join("\n")} >`;
+    });
+
+    
+    return {
+      dir: {
+        // ⚠️ These values are both relative to your input directory.
+        includes: "_includes",
+        layouts: "_includes/_layouts",
+        partials: "_includes/partials",
+        data: "_data",
+      }
+    }
+  
+};
---
+++ README.md
@@ -0,0 +1,23 @@
+# Letter from Birmingham Jail
+_By Dr. Martin Luther King, Jr._
+
+This project my own small contribution to Black Lives Matter and an effort to further my own education and growth. I wanted to be able to read this letter, but couldn't find a good version available online that wasn't buried in a PDF. I hope others will find it read it as well.
+
+Jason Pamental
+jason@rwt.io
+
+## Live URL
+
+The latest version of this code is automatically deployed to [Netlify](https://www.netlify.com/) and is findable at https://letterfromjail.com
+
+
+## Setup
+
+This text site is set up using the [static site generator Eleventy](https://www.11ty.dev/). If you want to use the build process, run the command `npm install`, and that should install the bits that you need (presuming you have Node and NPM installed already)
+
+To run the build process and serve the site locally, use this combination of commends to build the Sass files and build/serve the site: `gulp & npx @11ty/eleventy --serve`
+
+## Just the site
+
+If you want to just look at or work with the pre-built site, take a peek in the `_site` directory.
+
